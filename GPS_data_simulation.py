f0b56f8a31306cf0e2d6e7a2f593e4245fe251c6
danni <daw2039@med.cornell.edu>
2018-05-06 13:46:59 -0400

    kf+particle+choose

--- GPS_data_simulation.py
+++ GPS_data_simulation.py
@@ -54,3 +54,102 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# y and quaternion
+
+for i in range(N):
+    for j in range(rx_num-1):
+     quaternion_measurement[i] = N[i][j]*satelite_a.wavelength+satelite_a.wavelength*Model.doubleDiffRxSate[i][j]
+  
+# Karman filter 
+     from numpy import dot
+ 
+#X : The mean state estimate of the previous step ( k −1). P : The state covariance of previous step ( k −1).
+#A : The transition n × n matrix.
+#Q : The process noise covariance matrix.
+#B : The noise effect matrix. U : The control noise.    
+def kf_predict(X, P, A, Q, B, U):
+    X = dot(A, X) + dot(B, U)
+    P = dot(A, dot(P, A.T)) + Q
+    return(X,P)
+
+#K : the Kalman Gain matrix
+#IM : the Mean of predictive distribution of Y
+#IS : the Covariance or predictive mean of Y
+#LH : the Predictive probability (likelihood) of measurement which is computed using the Python function gauss_pdf
+from numpy import dot, sum, tile, linalg
+from numpy.linalg import inv
+
+def kf_update(X, P, Y, H, R): 
+    IM = dot(H, X)
+    IS = R + dot(H, dot(P, H.T)) 
+    K = dot(P, dot(H.T, inv(IS))) 
+    X = X + dot(K, (Y-IM))
+    P = P - dot(K, dot(IS, K.T)) 
+    LH = gauss_pdf(Y, IM, IS) 
+    return (X,P,K,IM,IS,LH)
+
+def gauss_pdf(X, M, S): 
+    if M.shape()[1] == 1:
+        DX = X - tile(M, X.shape()[1])
+        E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0)
+        E = E + 0.5 * M.shape()[0] * log(2 * pi) + 0.5 * log(det(S)) 
+        P = exp(-E)
+    elif X.shape()[1] == 1:
+        DX = tile(X, M.shape()[1])- M
+        E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0)
+        E = E + 0.5 * M.shape()[0] * log(2 * pi) + 0.5 * log(det(S))
+        P = exp(-E)
+    else:
+        DX = X-M
+        E = 0.5 * dot(DX.T, dot(inv(S), DX))
+        E = E + 0.5 * M.shape()[0] * log(2 * pi) + 0.5 * log(det(S)) 
+        P = exp(-E)
+    return (P[0],E[0])
+
+
+#particle filter
+    tau=100
+def particle(tau): 
+    x = np.zeros(tau) #initialize space
+    y = np.zeros(tau)
+    x[1] =0
+    ##add y[1]=x[1]+noise 
+    for i in range(2,tau):
+        #x[i]=x[i-1] state formular
+        #y[i]=x[i]#measurement formular
+    
+    #Begin particle filter
+    N=500
+    x_pf = np.zeros((tau+1,N))
+    #initialize
+    x_pf[0] = numpy.random.normal(size=N)
+    m = np.zeros(tau)
+    for t in range(2,(tau+1)):
+        #add x_pf[t] =x[t-1] state formular
+    
+    from scipy.stats import norm
+    #likelihood
+        w_tlide = norm.pdf(y[t-1],loc=,scale=wavelength**2)
+     #normalize
+    
+    w= w_tlide/sum(w_tlide)   
+    #estimate of state
+    m[t-1]=sum(w*x_pf[t])
+    
+    #resample step
+      s = numpy.random.choice(range(N), size=N, replace=True, p=w) 
+      x_pf =x_pf[s]
+ return(m)       
+
+   
+ #  use Karman or particle
+ import math
+ 
+    if math.cos(2*math.pi*sum(N))> 5.6:
+    #kalman filter
+else:
+    # particle filter
+
+
+     
